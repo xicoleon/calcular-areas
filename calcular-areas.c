6ec9beb8a3a48f072ca966d10771baea63945ceb
Francisco <xicoleon@gmail.com>
2018-08-08 20:30:16 -0400

    criacao do arquivo principal

---
+++ calcular-areas.c
@@ -0,0 +1,20 @@
+
+
+
+
+
+/*
+Oficina Git
+Professor: Orlewilson B. Maia
+Equipe: Francisco, Larissa, Diogo
+Data: 08/08/2018
+Descrição: Calcular area de circulo, triangulo e quadrado
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+	printf ("Hello World");
+	return 0;
+}
