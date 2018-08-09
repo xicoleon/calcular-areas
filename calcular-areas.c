45f01f7fc972c4986c0af8271997f3f24cea1c5f
Diogo <diocbrito@gmail.com>
2018-08-08 21:05:13 -0400

    Triangulo Equelátero

--- calcular-areas.c
+++ calcular-areas.c
@@ -14,7 +14,22 @@ Descrição: Calcular area de circulo, triangulo e quadrado
 #include <stdio.h>
 
 int main ()
+
 {
-	printf ("Hello World");
+
+int b;
+int h;
+int resultado;
+
+	printf("Digite sua base:");
+	scanf("%d", &b);
+
+	printf("Digite sua altura:");
+	scanf("%d", &h);
+	
+	resultado = b*h/2;
+
+	printf("Resultado: %d \n", resultado); 
+
 	return 0;
 }
