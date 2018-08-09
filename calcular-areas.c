82edac562a5c29a090c3097df1291a635e348397
Larissa <meu@email.com>
2018-08-08 21:11:53 -0400

    calculo quadrado

--- calcular-areas.c
+++ calcular-areas.c
@@ -1,8 +1,3 @@
-
-
-
-
-
 /*
 Oficina Git
 Professor: Orlewilson B. Maia
@@ -13,8 +8,26 @@ Descrição: Calcular area de circulo, triangulo e quadrado
 
 #include <stdio.h>
 
+int calcularquadrado (int b, int h)
+{
+	printf ("A area do quadrado será : %d\n", b*h);
+	return b*h;
+
+}
+
+
+
 int main ()
 {
-	printf ("Hello World");
+	int b, h, areaQ;
+	printf ("Hello World\n");
+	printf("Qual a base do quadrado?\n");
+	scanf ("%d", &b);
+	printf ("Qual a altura do quadrado?\n");
+	scanf ("%d", &h);	
+	areaQ = calcularquadrado (b, h);	
 	return 0;
 }
+
+
+
